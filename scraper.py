cefcfc112c7618dc8a2b572523dc1fb6dcf16a6c
Shawn Gazin <shawngazin@protonmail.com>
2026-04-21 14:39:43 -0700

    Co-authored-by: ThaiBenjamin <ThaiBenjamin@users.noreply.github.com>

--- scraper.py
+++ scraper.py
@@ -25,6 +25,10 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        
+        if parsed.netloc not in set(["*.ics.uci.edu/*", "*.cs.uci.edu/*", "*.informatics.uci.edu/*", "*.stat.uci.edu/*"]):
+            return False
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
