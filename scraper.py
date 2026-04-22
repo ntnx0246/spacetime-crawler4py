4ad216fb0d2e292151247837aefa357d48ca6506
Shawn G <shawngazin@protonmail.com>
2026-04-22 12:22:23 -0700

    Added global variables and conditions for valid urls
    Co-authored-by: ThaiBenjamin <ThaiBenjamin@users.noreply.github.com>

--- scraper.py
+++ scraper.py
@@ -1,6 +1,11 @@
 import re
 from urllib.parse import urlparse
 
+unique_pages = set() 
+longest_page = {"url":"", "word_count":0} #url, length
+word_frequencies = dict() #word: count
+subdomain_frequencies = dict() #subdomain: count
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -11,9 +16,9 @@ def extract_next_links(url, resp):
     print(f"Crawled: {url}")
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    if (resp.status != 200) {
+    if (resp.status != 200):
         return list()
-    }
+    
 
     # resp.error: when status is not 200, you can check the error here, if needed.
     print(resp.error)
@@ -33,8 +38,33 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
         
-        if parsed.netloc not in set(["*.ics.uci.edu/*", "*.cs.uci.edu/*", "*.informatics.uci.edu/*", "*.stat.uci.edu/*"]):
+
+        #Check if the url is in the list of valid domains
+        valid_domains = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu"]
+        valid = False
+        
+        for domain in valid_domains:
+            #First condition checks for subdomains, second condition checks for the domain itself
+            if parsed.netloc.endswith(domain) or parsed.netloc == domain[1:]:
+                valid = True
+                break
+
+        if not valid:
             return False
+
+        #Check if the path is too long to avoid infinite trap
+        if len(parsed.path) > 400:
+            return False
+
+        #Check for duplicate paths to avoid infinite trap
+        path_segments = parsed.path.strip("/").split("/")
+
+        #Detects whether a duplicate path exists
+        if len(path_segments) != len(set(path_segments)):
+            #Allow for certain duplicate paths that can happen because of chance in a valid url
+            if len(path_segments) >= 5 and len(set(path_segments)) < len(path_segments) - 2:
+                return False
+        
         
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
